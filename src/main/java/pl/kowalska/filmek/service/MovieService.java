38fab80bf96a7c576ac94889d996a729716a0c9e
EwBu <ewelina_bula@outlook.com>
2021-02-15 18:01:35 +0100

    update

--- src/main/java/pl/kowalska/filmek/service/MovieService.java
+++ src/main/java/pl/kowalska/filmek/service/MovieService.java
@@ -1,9 +1,14 @@
 package pl.kowalska.filmek.service;
 
 import org.springframework.stereotype.Service;
+import pl.kowalska.filmek.model.MovieEntity;
 
 @Service
 public class MovieService {
 
-
+    public MovieEntity findMovieById(Long movieId){
+        MovieEntity movie = new MovieEntity();
+        System.out.println("metoda find Movie By Id");
+        return movie;
+    }
 }
