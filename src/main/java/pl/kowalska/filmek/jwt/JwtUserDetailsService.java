afda5fe78854edd5d5ef477ef0e0505b3aa29db1
simplycodestudio <simplycodestudio@gmail.com>
2021-02-23 18:50:04 +0100

    first update

---
+++ src/main/java/pl/kowalska/filmek/jwt/JwtUserDetailsService.java
@@ -0,0 +1,41 @@
+package pl.kowalska.filmek.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import pl.kowalska.filmek.model.User;
+import pl.kowalska.filmek.model.UserDTO;
+import pl.kowalska.filmek.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@Component
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public User save(UserDTO user) {
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return userRepository.save(newUser);
+    }
+
+}
