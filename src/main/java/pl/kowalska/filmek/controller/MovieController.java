503a03abaaeef6c737f16fb41da818ede387d3b7
simplycodestudio <simplycodestudio@gmail.com>
2021-03-03 20:05:52 +0100

    security

--- src/main/java/pl/kowalska/filmek/controller/MovieController.java
+++ src/main/java/pl/kowalska/filmek/controller/MovieController.java
@@ -1,6 +1,8 @@
 package pl.kowalska.filmek.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +34,11 @@ public class MovieController {
 
     @GetMapping("/{movieId}")
     public String viewMovieDetail(Model model, @PathVariable Long movieId){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+
+
         MovieEntity selectedMovie = movieService.findSingleMovieInDatabase(movieId);
         if (selectedMovie!=null){
             model.addAttribute("film", selectedMovie);
